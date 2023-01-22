de52112c97e7f2eceee0ac244e4313f6c639fc14
DESKTOP-V0QPLFB\andre <1freixasandreas@gmail.com>
2023-01-22 14:34:53 +0100

    actualizacion de ciheros package y creado conexion de prueba a mysql

---
+++ pruebamysql.js
@@ -0,0 +1,23 @@
+
+const mysql = require('mysql2');
+
+
+const conn = mysql.createConnection({
+
+
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Masnou24796*',
+    port: 3306,
+    database: 'agencia'
+});
+
+
+
+
+conn.connect((err) => { console.log(err) })
+conn.query('SELECT * FROM agencia.clientes', (err, result) => {
+    console.log(err);
+    console.log(result);
+})
+
