b932804b7bad625462f53cb57e9fe342ad49a766
DESKTOP-V0QPLFB\andre <1freixasandreas@gmail.com>
2023-01-23 10:46:13 +0100

    creada la carpeta models con sus respectivos archivos

---
+++ routes/api/clientes.js
@@ -0,0 +1,10 @@
+
+
+// router.get('/', async(req,res)=>{
+//     try{
+//         const[result] = await getAll()
+//         res.json(result);
+//     }catch(error){
+//         res.json({fatal: error.message});
+//     }
+// });
