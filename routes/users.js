514ebd2ee9250e29edda130e2e6ee277ee327603
DESKTOP-V0QPLFB\andre <1freixasandreas@gmail.com>
2023-01-24 12:44:09 +0100

    creado archivo api, modificado el archivo db, requerido b en bin www y optimizado las peticiones get del archivo users

--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,15 @@ const { connect } = require('.');
 var router = express.Router();
 const mysql = require('mysql2');
 
-const conn = mysql.createConnection({
-
-
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'root',
-  port: 3306,
-  database: 'agencia'
-});
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  conn.connect((err)=>{
-    conn.query('SELECT * FROM clientes', (err, result)=>{
-      res.json(result)
-    })
+  db.query('SELECT * FROM clientes')
+  .then((result) => {
+    res.json(result[0]);
   })
+  .catch((error) => {
+    console.log(error);
+  });
 });
 
 module.exports = router;
@@ -27,10 +19,14 @@ module.exports = router;
 //GET clietnes por id
 router.get('/:clientesId',(req, res)=>{
   const clientesId = parseInt(req.params.clientesId);
-  conn.connect((err)=>{
-    conn.query('SELECT * FROM clientes WHERE clientes.id=?',
-    [clientesId], (err, result)=>{
-      res.json(result)
-    })
+  db.query('SELECT * FROM clientes WHERE clientes.id=?', [clientesId])
+  .then((result) => {
+    res.json(result[0]);
   })
+  .catch((error) => {
+    console.log(error);
+  });
+
 })
+
+//put 
